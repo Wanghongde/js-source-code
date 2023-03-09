f8542ac3274dc72450734dda849fdcc2cf53c537
王洪德 <315020798@qq.com>
2023-03-09 16:07:56 +0800

    v-model绑定对象属性

--- 07-vue2_mvvm.js
+++ 07-vue2_mvvm.js
@@ -1,7 +1,6 @@
-function Vue2(options = {}) {
+function Vue(options = {}) {
   this.$options = options
-
-  let data = this._data = this.$options.data
+  let data = this._data = options.data
 
   observe(data)
 
@@ -12,97 +11,37 @@ function Vue2(options = {}) {
         return data[key]
       },
       set(newVal) {
+        if(data[key] === newVal) return 
         data[key] = newVal
       }
     })
   }
-
+  
   initComputed.call(this)
 
-  new Compile(options.el, this) 
-}
-
-function Compile(el, vm) {
-  let app = document.querySelector(el)
-
-  let fragment = document.createDocumentFragment()
-
-  while(child = app.firstChild) {
-    fragment.appendChild(child)
-  }
-
-  function replace(fragment) {
-    Array.from(fragment.childNodes).forEach(node => {
-      let text = node.textContent
-      let reg = /\{\{\s*(\S*)\s*\}\}/g
-
-      if(node.nodeType === 3 && reg.test(text)) {
-        let arr = RegExp.$1.split('.')
-        let val = vm
-
-        arr.forEach(key => {
-          val = val[key]
-        })
-
-        new Watcher(vm, RegExp.$1, function(newVal) {
-          node.textContent = text.replace(/\{\{\s*(.*)\s*\}\}/g, newVal)
-        })
-
-        node.textContent = node.textContent.replace(/\{\{\s*(.*)\s*\}\}/g, val)
-      }
-
-      if(node.nodeType === 1) {
-        let nodeAttrs = node.attributes
-
-        Array.from(nodeAttrs).forEach(attr => {
-          if(attr.name.includes('v-model')) {
-            node.value = vm[attr.value]
-
-            new Watcher(vm, attr.value, function(newVal) {
-              node.value = newVal
-            })
-
-            node.addEventListener('input', function(e) {
-              vm[attr.value] = e.target.value
-            })
-          }
-        })
-       
-      }
-
-      if(node.childNodes) {
-        replace(node)
-      }
-
-    })
-  }
-
-  replace(fragment)
-
-  app.appendChild(fragment)
+  new Compile(options.el, this)
 }
 
 function Observe(data) {
   let dep = new Dep()
-
   for(let key in data) {
     let value = data[key]
-
     observe(value)
-  
+
     Object.defineProperty(data, key, {
       enumerable: true,
       get() {
-        Dep.target && dep.addList(Dep.target)
+        Dep.target && dep.addSub(Dep.target)
+
         return value
       },
       set(newVal) {
-        if(newVal === value) {
-          return 
-        }
+        if(value === newVal) return 
 
         value = newVal
+
         observe(newVal)
+
         dep.notify()
       }
     })
@@ -110,15 +49,80 @@ function Observe(data) {
 }
 
 function observe(data) {
-  if(typeof data !== 'object') return
-  return new Observe(data)
+  if(typeof data !== 'object') return 
+  new Observe(data)
+}
+
+function Compile(el, vm) {
+  let app = document.querySelector(el)
+  let fragment = document.createDocumentFragment()
+  let reg = /\{\{\s*(\S*)\s*\}\}/g
+
+  while(first = app.firstChild) {
+    fragment.appendChild(first)
+  }
+
+  replace(fragment)
+
+  function replace(fragment) {
+    Array.from(fragment.childNodes).forEach(node => {
+      if(node.nodeType === 3 && reg.test(node.textContent)) {
+        let arr = RegExp.$1.split('.')
+        let val = vm
+        let text = node.textContent
+        arr.forEach(key => val = val[key])
+
+        node.textContent = node.textContent.replace(reg, val)
+
+        new Watcher(vm, RegExp.$1, function(newVal) {
+          node.textContent = text.replace(reg, newVal)  // 这里要拿到修改后的新数据
+        })
+      }
+  
+      if(node.nodeType === 1) {
+        Array.from(node.attributes).forEach(item => {
+          if(item.name === 'v-model') {
+            let arr = item.textContent.split('.')
+            let val = vm
+
+            arr.forEach((key, index) => {
+              if(index === arr.length - 1) {
+                new Watcher(vm, item.textContent, function(newVal) {
+                  node.value = val[key]
+                })
+                return node.value = val[key]
+              }
+              val = val[key]
+            })
+
+            node.addEventListener('input', e => {
+              let val = vm
+              arr.forEach((key, index) => {
+                if(index === arr.length - 1) {
+                  return val[key] = e.target.value
+                }
+                val = val[key]
+              })
+            })
+            
+          }
+        })
+      }
+
+      if(node.childNodes) {
+        replace(node)
+      }
+    })
+  }
+
+  app.append(fragment)
 }
 
 function Dep() {
   this.list = []
 }
 
-Dep.prototype.addList = function(sub){
+Dep.prototype.addSub = function(sub) {
   this.list.push(sub)
 }
 
@@ -128,37 +132,26 @@ Dep.prototype.notify = function() {
   })
 }
 
-function Watcher(vm, exp, fn) {
+function Watcher(vm, expression, fn) {
   this.vm = vm
-  this.exp = exp
+  this.expression = expression
   this.fn = fn
 
   Dep.target = this
-
   let val = vm
-  let arr = exp.split('.')
-  arr.forEach(key => {
-    val = val[key]
-  })
-
+  expression.split('.').forEach(key => val[key])
   Dep.target = null
-
 }
 
-Watcher.prototype.update = function(){
+Watcher.prototype.update = function() {
   let val = this.vm
-  let arr = this.exp.split('.')
-
-  arr.forEach(key => {
-    val = val[key]
-  })
+  this.expression.split('.').forEach(key => val = val[key])
 
   this.fn(val)
 }
 
 function initComputed() {
   let computed = this.$options.computed
-
   Object.keys(computed).forEach(key => {
     console.log(this, key)
     Object.defineProperty(this, key, {
@@ -166,5 +159,4 @@ function initComputed() {
       set() {}
     })
   })
-
 }
